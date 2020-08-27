ab0b508145330e0db33564a5c257114a1ef0e567
Samitti <samuelteweldebrhan@gmail.com>
2020-08-27 18:15:26 +0300

    add form to index page and functionality to the index.js

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(title, author, numPages, readStatus) {
-  this.title = title
-  this.author = author
-  this.numPages = numPages
-  this.readStatus = readStatus
-  this.bookInfo = function () {
-    return `${title} ${author}, ${numPages}, ${readStatus}`;
-  }
+  this.title = title;
+  this.author = author;
+  this.numPages = numPages;
+  this.readStatus = readStatus;
 }
 
-function addBookToLibrary() {
-  
+function addBookToLibrary(ev) {
+  ev.preventDefault();
+  const book = new Book();
+  book.title = document.getElementById("title").value;
+  book.author = document.getElementById("author").value;
+  book.numPages = document.getElementById("numPages").value;
+  book.readStatus = document.getElementById("readStatus").value;
+
+  myLibrary.push(book);
+  const pre = document.querySelector("#msg pre");
+  pre.textContent = `\n${JSON.stringify(myLibrary, "\t", 2)}`;
 }
+
+console.log(myLibrary);
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("submit").addEventListener("click", addBookToLibrary);
+});
