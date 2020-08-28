5005517852d1678fc0384b872780904d8aec2f9b
Samitti <samuelteweldebrhan@gmail.com>
2020-08-28 12:04:19 +0300

    add render function

--- index.js
+++ index.js
@@ -7,21 +7,34 @@ function Book(title, author, numPages, readStatus) {
   this.readStatus = readStatus;
 }
 
+// test dynamic table
+function render(book) {
+  const list = document.querySelector('#book-list');
+  const row = document.createElement('tr');
+  row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.numPages}</td>
+  <td>${book.readStatus}</td>
+  `;
+  list.appendChild(row);
+}
+
+// test dynamic table
 function addBookToLibrary(ev) {
   ev.preventDefault();
   const book = new Book();
-  book.title = document.getElementById("title").value;
-  book.author = document.getElementById("author").value;
-  book.numPages = document.getElementById("numPages").value;
-  book.readStatus = document.getElementById("readStatus").value;
+  book.title = document.getElementById('title').value;
+  book.author = document.getElementById('author').value;
+  book.numPages = document.getElementById('numPages').value;
+  book.readStatus = document.getElementById('readStatus').value;
 
   myLibrary.push(book);
-  const pre = document.querySelector("#msg pre");
-  pre.textContent = `\n${JSON.stringify(myLibrary, "\t", 2)}`;
+  render(book);
+  // const pre = document.querySelector('#msg pre');
+  // pre.textContent = `\n${JSON.stringify(myLibrary, '\t', 2)}`;
 }
 
-console.log(myLibrary);
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("submit").addEventListener("click", addBookToLibrary);
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('submit').addEventListener('click', addBookToLibrary);
 });
