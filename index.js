ca0619e8648eeed792cf6b35142ca53b5af8fd3d
Samitti <samuelteweldebrhan@gmail.com>
2020-08-31 17:27:45 +0300

    add functionality to the change status btn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const myLibrary = [
-  {
-    title: 'Book One',
-    author: 'Samuel',
-    numPages: 200,
-    readStatus: 'read'
-  }
-];
+const myLibrary = [{
+  title: 'Book One',
+  author: 'Samuel',
+  numPages: 200,
+  readStatus: 'Already Read',
+}];
 
 function Book(title, author, numPages, readStatus) {
   this.title = title;
@@ -14,7 +12,6 @@ function Book(title, author, numPages, readStatus) {
   this.readStatus = readStatus;
 }
 
-// test dynamic table
 function displayBook(book) {
   const list = document.querySelector('#book-list');
   const row = document.createElement('tr');
@@ -22,7 +19,7 @@ function displayBook(book) {
   <td>${book.title}</td>
   <td>${book.author}</td>
   <td>${book.numPages}</td>
-  <td>${book.readStatus}<a href="#" class="btn btn-danger btn-sm ml-4 delete">Change Status</a></td>
+  <td><span class="read-status">${book.readStatus}</span><a href="#"  class="btn btn-danger btn-sm ml-4 update" >Change Status</a></td>
   <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
   `;
   list.appendChild(row);
@@ -34,7 +31,7 @@ function render() {
   });
 }
 
-// test dynamic table
+
 function addBookToLibrary(ev) {
   ev.preventDefault();
   const book = new Book();
@@ -45,14 +42,14 @@ function addBookToLibrary(ev) {
 
   myLibrary.push(book);
   displayBook(book);
-  
-  // const pre = document.querySelector('#msg pre');
-  // pre.textContent = `\n${JSON.stringify(myLibrary, '\t', 2)}`;
+
+  const pre = document.querySelector('#msg pre');
+  pre.textContent = `\n${JSON.stringify(myLibrary, '\t', 2)}`;
 }
 
 function newBookEntry() {
-  let x = document.getElementById('myForm');
-  if (x.style.display === "block") {
+  const x = document.getElementById('myForm');
+  if (x.style.display === 'block') {
     x.style.display = 'none';
   } else {
     x.style.display = 'block';
@@ -60,27 +57,39 @@ function newBookEntry() {
 }
 
 function hideNewBookButton() {
-  let x = document.getElementById('btn');
-  if (x.style.display === "none") {
+  newBookEntry();
+  const x = document.getElementById('btn');
+  if (x.style.display === 'none') {
     x.style.display = 'block';
   } else {
     x.style.display = 'none';
   }
 }
 
+function changeReadStatus(el) {
+  const parent = el.parentElement;
+  const status = parent.querySelector('.read-status');
+  const index = el.parentElement.parentElement.rowIndex;
+  if (status.innerHTML !== 'Already Read') {
+    status.innerHTML = 'Already Read';
+    myLibrary[index - 1].readStatus = 'Already Read';
+  } else {
+    status.innerHTML = 'Not Read';
+    myLibrary[index - 1].readStatus = 'Not Read';
+  }
+}
+
 function deleteBook(el) {
-  if(el.classList.contains('delete')) {
-    el.parentElement.parentElement.remove();
-  }
-}
-
-function updateReadStatus(el) {
   if (el.classList.contains('delete')) {
+    const remindex = el.parentElement.parentElement.rowIndex;
+    const startpos = remindex - 1;
+    myLibrary.splice(startpos, 1);
     el.parentElement.parentElement.remove();
+  } else if (el.classList.contains('update')) {
+    changeReadStatus(el);
   }
 }
 
-// document.addEventListener('DOMContentLoaded', );
 
 document.addEventListener('DOMContentLoaded', () => {
   render();
@@ -88,9 +97,9 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 document.getElementById('book-list').addEventListener('click', (e) => {
-  deleteBook(e.target)
-})
+  deleteBook(e.target);
+});
 
 document.getElementById('btn').addEventListener('click', (e) => {
-  hideNewBookButton(e.target)
-}) 
+  hideNewBookButton(e.target);
+});
